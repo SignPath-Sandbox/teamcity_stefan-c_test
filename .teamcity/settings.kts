4b6b7cb40d5937e80e032381c9cd48b98a754709
SteveTarango <48215267+SteveTarango@users.noreply.github.com>
2025-03-31 14:07:22 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -24,7 +24,7 @@ project {
         apiToken = "credentialsJSON:a03ec855-c92c-4f33-8877-b8ab1726afd4"
         projectSlug = "Stefans_Teamcity_Project"
         signingPolicySlug = "test-signing"
-        inputArtifactPath = "Loop.ps1"
+        inputArtifactPath = "Loop.ps2"
         outputArtifactPath = "Loop-signed.ps1"
         waitForCompletion = true
       }
