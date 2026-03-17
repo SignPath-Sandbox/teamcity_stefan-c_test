b03464172c711723b352931d4696659a3287ba2d
SteveTarango <48215267+SteveTarango@users.noreply.github.com>
2026-03-17 10:29:06 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -30,7 +30,7 @@ project {
         artifactConfigurationSlug = "initial"
         inputArtifactPath = "Loop.ps1"
         outputArtifactPath = "Loop-signed.ps1"
-        waitForCompletion = false
+        waitForCompletion = true
          parameters = """
                     version: 1.0.0
                     other_param: other value
