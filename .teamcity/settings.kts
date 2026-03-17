2ba6d3bd025c19dd2342dd7d5fc2749fe23530a5
SteveTarango <48215267+SteveTarango@users.noreply.github.com>
2026-03-17 13:20:27 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -24,7 +24,7 @@ project {
       signPathSubmitSigningRequest {
         connectorUrl = "https://pipeline-connector-stable.customersimulation.int.signpath.io/TeamCity/TeamCityServer"     
         organizationId = "9ff791fc-c563-44e3-ab8c-86a33c910bbe"
-        apiToken = "credentialsJSON:a03ec855-c92c-4f33-8877-b8ab1726afdä"
+        apiToken = "credentialsJSON:a03ec855-c92c-4f33-8877-b8ab1726afd4"
         projectSlug = "Stefans_Teamcity_Project"
         signingPolicySlug = "test-signing"
         artifactConfigurationSlug = "initial"
